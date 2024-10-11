f625c37798ccf7d3bae5175c91c9825b47d2db6e
TOMCHRISTER\tomch <tomsch01266@stud.noroff.no>
2024-10-11 09:04:13 +0200

    minor fix

--- calculator.js
+++ calculator.js
@@ -8,6 +8,7 @@ export function minus(num1, num2) {
 }
 minus();
 
+
 export function divide(num1, num2) {
     if (num2 === 0) {
         throw Error('Cant divide by 0');
--- calculator.test.js
+++ calculator.test.js
@@ -1,15 +1,4 @@
-import { add } from "./index.js";
 import { divide } from './calculator.js';
-
-test("It adds one and one and returns two", () => {
-    // arrange
-    const param1 = 1;
-    const param2 = 1;
-    const expected = 2;
-
-    // act and assert
-    expect(add(param1, param2)).toEqual(expected);
-});
 
 test('Should throw a error when divided by 0', () => {
     // arrange
